d0d0ef3cadd48c916c7b9c619a18ac5a30984c54
kircanski023 <kircanskis@gmail.com>
2025-03-30 22:47:53 +0200

    My solutions final

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(arr) {
+    return arr
+     .map((obj) => {
+       if(obj.yearOfDeath === undefined){
+         return {
+         name: obj.name,
+         yearOfBirth: obj.yearOfBirth,
+         yearsOld: new Date().getFullYear() - obj.yearOfBirth,
+       }
+       }
+       else{
+         return {
+         name: obj.name,
+         yearOfBirth: obj.yearOfBirth,
+         yearOfDeath: obj.yearOfDeath,
+         yearsOld: obj.yearOfDeath - obj.yearOfBirth,
+       }
+       }
+     })
+     .sort((a, b) => a.yearsOld - b.yearsOld)
+     .pop();
+ 
+   }
 // Do not edit below this line
 module.exports = findTheOldest;
